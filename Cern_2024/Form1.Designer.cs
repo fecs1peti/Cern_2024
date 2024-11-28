0ff903cedb09a8d07bd4e76951d7b0999122790a
Fecso.Peter <Fecso.Peter@C19-09>
2024-11-28 20:19:01 +0100

    Add project files.

---
+++ Cern_2024/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Cern_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_VJobbAlsó = new System.Windows.Forms.PictureBox();
+            this.pb_Akku = new System.Windows.Forms.PictureBox();
+            this.pb_VJobb = new System.Windows.Forms.PictureBox();
+            this.pb_Lámpa = new System.Windows.Forms.PictureBox();
+            this.pb_VFelső = new System.Windows.Forms.PictureBox();
+            this.pb_VBal = new System.Windows.Forms.PictureBox();
+            this.pb_VBalAlsó = new System.Windows.Forms.PictureBox();
+            this.l_e_db = new System.Windows.Forms.Label();
+            this.pb_Fesz_Szab = new System.Windows.Forms.PictureBox();
+            this.l_AktFesz = new System.Windows.Forms.Label();
+            this.b_VDown = new System.Windows.Forms.Button();
+            this.b_VUp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VJobbAlsó)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Akku)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VJobb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Lámpa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VFelső)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VBal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VBalAlsó)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Fesz_Szab)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_VJobbAlsó
+            // 
+            this.pb_VJobbAlsó.BackColor = System.Drawing.Color.Blue;
+            this.pb_VJobbAlsó.Location = new System.Drawing.Point(676, 422);
+            this.pb_VJobbAlsó.Name = "pb_VJobbAlsó";
+            this.pb_VJobbAlsó.Size = new System.Drawing.Size(50, 4);
+            this.pb_VJobbAlsó.TabIndex = 2;
+            this.pb_VJobbAlsó.TabStop = false;
+            // 
+            // pb_Akku
+            // 
+            this.pb_Akku.BackgroundImage = global::Cern_2024.Properties.Resources.akkumulator;
+            this.pb_Akku.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_Akku.Location = new System.Drawing.Point(606, 428);
+            this.pb_Akku.Name = "pb_Akku";
+            this.pb_Akku.Size = new System.Drawing.Size(79, 49);
+            this.pb_Akku.TabIndex = 1;
+            this.pb_Akku.TabStop = false;
+            // 
+            // pb_VJobb
+            // 
+            this.pb_VJobb.BackColor = System.Drawing.Color.Blue;
+            this.pb_VJobb.Location = new System.Drawing.Point(722, 176);
+            this.pb_VJobb.Name = "pb_VJobb";
+            this.pb_VJobb.Size = new System.Drawing.Size(4, 250);
+            this.pb_VJobb.TabIndex = 0;
+            this.pb_VJobb.TabStop = false;
+            // 
+            // pb_Lámpa
+            // 
+            this.pb_Lámpa.BackgroundImage = global::Cern_2024.Properties.Resources.sarga_korte;
+            this.pb_Lámpa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_Lámpa.Location = new System.Drawing.Point(185, 117);
+            this.pb_Lámpa.Name = "pb_Lámpa";
+            this.pb_Lámpa.Size = new System.Drawing.Size(66, 84);
+            this.pb_Lámpa.TabIndex = 7;
+            this.pb_Lámpa.TabStop = false;
+            // 
+            // pb_VFelső
+            // 
+            this.pb_VFelső.BackColor = System.Drawing.Color.Blue;
+            this.pb_VFelső.Location = new System.Drawing.Point(226, 176);
+            this.pb_VFelső.Name = "pb_VFelső";
+            this.pb_VFelső.Size = new System.Drawing.Size(500, 4);
+            this.pb_VFelső.TabIndex = 8;
+            this.pb_VFelső.TabStop = false;
+            // 
+            // pb_VBal
+            // 
+            this.pb_VBal.BackColor = System.Drawing.Color.Red;
+            this.pb_VBal.Location = new System.Drawing.Point(216, 192);
+            this.pb_VBal.Name = "pb_VBal";
+            this.pb_VBal.Size = new System.Drawing.Size(4, 234);
+            this.pb_VBal.TabIndex = 9;
+            this.pb_VBal.TabStop = false;
+            // 
+            // pb_VBalAlsó
+            // 
+            this.pb_VBalAlsó.BackColor = System.Drawing.Color.Red;
+            this.pb_VBalAlsó.Location = new System.Drawing.Point(216, 422);
+            this.pb_VBalAlsó.Name = "pb_VBalAlsó";
+            this.pb_VBalAlsó.Size = new System.Drawing.Size(400, 4);
+            this.pb_VBalAlsó.TabIndex = 10;
+            this.pb_VBalAlsó.TabStop = false;
+            // 
+            // l_e_db
+            // 
+            this.l_e_db.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.l_e_db.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.l_e_db.Location = new System.Drawing.Point(573, 47);
+            this.l_e_db.Name = "l_e_db";
+            this.l_e_db.Size = new System.Drawing.Size(153, 23);
+            this.l_e_db.TabIndex = 11;
+            this.l_e_db.Text = "Electron Passed:";
+            this.l_e_db.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pb_Fesz_Szab
+            // 
+            this.pb_Fesz_Szab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.pb_Fesz_Szab.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_Fesz_Szab.Location = new System.Drawing.Point(401, 131);
+            this.pb_Fesz_Szab.Name = "pb_Fesz_Szab";
+            this.pb_Fesz_Szab.Size = new System.Drawing.Size(78, 86);
+            this.pb_Fesz_Szab.TabIndex = 12;
+            this.pb_Fesz_Szab.TabStop = false;
+            // 
+            // l_AktFesz
+            // 
+            this.l_AktFesz.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.l_AktFesz.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.l_AktFesz.Location = new System.Drawing.Point(411, 141);
+            this.l_AktFesz.Name = "l_AktFesz";
+            this.l_AktFesz.Size = new System.Drawing.Size(58, 23);
+            this.l_AktFesz.TabIndex = 13;
+            this.l_AktFesz.Text = "12V";
+            this.l_AktFesz.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // b_VDown
+            // 
+            this.b_VDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b_VDown.Location = new System.Drawing.Point(420, 192);
+            this.b_VDown.Name = "b_VDown";
+            this.b_VDown.Size = new System.Drawing.Size(40, 20);
+            this.b_VDown.TabIndex = 14;
+            this.b_VDown.Text = "\\/";
+            this.b_VDown.UseVisualStyleBackColor = true;
+            this.b_VDown.Click += new System.EventHandler(this.b_VDown_Click);
+            // 
+            // b_VUp
+            // 
+            this.b_VUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b_VUp.Location = new System.Drawing.Point(420, 169);
+            this.b_VUp.Name = "b_VUp";
+            this.b_VUp.Size = new System.Drawing.Size(40, 20);
+            this.b_VUp.TabIndex = 15;
+            this.b_VUp.Text = "/\\";
+            this.b_VUp.UseVisualStyleBackColor = true;
+            this.b_VUp.Click += new System.EventHandler(this.b_VUp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 500);
+            this.Controls.Add(this.b_VUp);
+            this.Controls.Add(this.b_VDown);
+            this.Controls.Add(this.l_AktFesz);
+            this.Controls.Add(this.pb_Fesz_Szab);
+            this.Controls.Add(this.l_e_db);
+            this.Controls.Add(this.pb_VBalAlsó);
+            this.Controls.Add(this.pb_VBal);
+            this.Controls.Add(this.pb_VFelső);
+            this.Controls.Add(this.pb_Lámpa);
+            this.Controls.Add(this.pb_VJobbAlsó);
+            this.Controls.Add(this.pb_Akku);
+            this.Controls.Add(this.pb_VJobb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VJobbAlsó)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Akku)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VJobb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Lámpa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VFelső)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VBal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_VBalAlsó)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Fesz_Szab)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_VJobb;
+        private System.Windows.Forms.PictureBox pb_Akku;
+        private System.Windows.Forms.PictureBox pb_VJobbAlsó;
+        private System.Windows.Forms.PictureBox pb_Lámpa;
+        private System.Windows.Forms.PictureBox pb_VFelső;
+        private System.Windows.Forms.PictureBox pb_VBal;
+        private System.Windows.Forms.PictureBox pb_VBalAlsó;
+        private System.Windows.Forms.Label l_e_db;
+        private System.Windows.Forms.PictureBox pb_Fesz_Szab;
+        private System.Windows.Forms.Label l_AktFesz;
+        private System.Windows.Forms.Button b_VDown;
+        private System.Windows.Forms.Button b_VUp;
+    }
+}
+
---
+++ Cern_2024/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cern_2024
+{
+    public partial class Form1 : Form
+    {
+        int v_akt = 12, e_db = 0;
+        int vj_l, vf_t, vb_l, va_t;
+        List<Label> e_all = new List<Label>();
+        Timer new_e = new Timer();
+        Timer move_e = new Timer();
+        string a_i;
+        List<int> del_all = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+
+        }
+        void Start()
+        {
+            E_500ms();
+            //New_Electron();
+            E_Moving();
+
+        }
+
+        void E_500ms()
+        {
+            new_e.Interval = 500;
+            new_e.Start();
+            new_e.Tick += (sb, eb) =>
+            {
+                New_Electron();
+            };
+        }
+        void New_Electron()
+        {
+            Label uj = new Label();
+            uj.Width = 10;
+            uj.Height = 10;
+            uj.BackColor = Color.Yellow;
+            uj.ForeColor = Color.Yellow;
+            uj.BorderStyle = BorderStyle.FixedSingle;
+            uj.Text = "01";
+            uj.TabIndex = 24;
+            uj.Top = pb_VJobb.Bottom - 7;
+            uj.Left = pb_VJobb.Left - 3;
+            this.Controls.Add(uj);
+            uj.BringToFront();
+            e_all.Add(uj);
+            pb_Fesz_Szab.BringToFront();
+            l_AktFesz.BringToFront();
+            b_VUp.BringToFront();
+            b_VDown.BringToFront();
+        }
+        void E_Moving()
+        {
+            move_e.Interval = 30;
+            move_e.Tick += (sm, em) =>
+            {
+                for (int i = 0; i < e_all.Count; i++)
+                {
+                    if (e_all[i].Visible==true)
+                    {
+                        int a_x = e_all[i].Left;
+                        int a_y = e_all[i].Top;
+                        a_i = e_all[i].Text;
+                        if (a_i == "01")
+                        {
+                            if (a_y > pb_VJobb.Top + e_all[i].TabIndex)
+                            {
+                                e_all[i].Top -= e_all[i].TabIndex;
+
+                            }
+                            else
+                            {
+                                e_all[i].Text = "-10";
+                                e_all[i].Top = pb_VJobb.Top - 3;
+                            }
+                        }
+                        else if (a_i == "-10")
+                        {
+                            if (a_x > pb_VBal.Left + e_all[i].TabIndex)
+                            {
+                                e_all[i].Left -= e_all[i].TabIndex;
+                            }
+                            else
+                            {
+                                e_all[i].Text = "0-1";
+                                e_all[i].Left = pb_VBal.Left - 3;
+                            }
+                            if (e_all[i].Left > pb_Fesz_Szab.Left && e_all[i].Left < pb_Fesz_Szab.Right)
+                            {
+                                e_all[i].TabIndex = v_akt;
+                            }
+                        }
+                        else if (a_i == "0-1")
+                        {
+                            if (a_y < pb_VJobb.Bottom - e_all[i].TabIndex)
+                            {
+                                e_all[i].Top += e_all[i].TabIndex;
+                            }
+                            else
+                            {
+                                e_all[i].Text = "10";
+                                e_all[i].Top = pb_VJobbAlsó.Top - 3;
+                            }
+                        }
+                        else if (a_i == "10")
+                        {
+                            if (a_x < pb_VBalAlsó.Right - e_all[i].TabIndex)
+                            {
+                                e_all[i].Left += e_all[i].TabIndex;
+                            }
+                            else
+                            {
+                                del_all.Add(i);
+                                e_all[i].Hide();
+
+                                e_db++;
+                                l_e_db.Text = "Electron Passsed: " + e_db.ToString();
+                            }
+                        };
+                    }
+                    /*if (del_all.Count > 0)
+                    {
+                        e_all.RemoveAt(del_all[0]);
+                        del_all.RemoveAt(0);
+                    }*/
+                }
+            };
+            move_e.Start();
+        }
+
+        private void b_VUp_Click(object sender, EventArgs e)
+        {
+            if (v_akt < 24)
+            {
+                v_akt++;
+            }
+            l_AktFesz.Text = v_akt.ToString() + "V";
+        }
+
+        private void b_VDown_Click(object sender, EventArgs e)
+        {
+            if (v_akt > 1)
+            {
+                v_akt--;
+            }
+            l_AktFesz.Text = v_akt.ToString() + "V";
+        }
+    }
+}
